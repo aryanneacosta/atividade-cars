10676710cfbfa8a750408edf208816b9540c71ac
Aryanne Acosta <aryanneacosta@gmail.com>
2023-01-26 16:39:11 -0300

    atividade cars

---
+++ src/repository/carRepository.ts
@@ -0,0 +1,44 @@
+import prisma from "../config/database.js";
+
+async function getCars() {
+ return prisma.cars.findMany();
+}
+
+async function getCar(id: number) {
+  return prisma.cars.findFirst({
+    where: { id }
+  });
+}
+
+async function getCarWithLicensePlate(licensePlate: string) {
+  return prisma.cars.findFirst({
+    where: { licensePlate }
+  });
+}
+
+async function createCar(model: string, licensePlate: string, year: number, color: string) {
+  return prisma.cars.create({
+    data: {
+      model,
+      licensePlate,
+      year,
+      color
+    }
+  });
+}
+
+async function deleteCar(id: number) {
+  return prisma.cars.delete({
+    where: { id }
+  });
+}
+
+const carRepository = {
+  getCar,
+  getCarWithLicensePlate,
+  getCars,
+  createCar,
+  deleteCar
+}
+
+export default carRepository;
